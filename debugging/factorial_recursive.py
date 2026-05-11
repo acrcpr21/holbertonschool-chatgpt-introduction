34dd0fad45128609f32e7253f67f19799d9bb5fe
acrcpr21 <acrcpr@gmail.com>
2026-05-11 16:20:31 -0400

    Implemented a recursive approach to calculate factorials

---
+++ debugging/factorial_recursive.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+import sys
+
+def factorial(n):
+    """
+    Calculate the factorial of a non-negative integer n using recursion.
+    
+    Parameters:
+    n (int): A non-negative integer for which the factorial is to be calculated.
+    
+    Returns:
+    int: The factorial of the input integer n.
+    """
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n-1)
+
+f = factorial(int(sys.argv[1]))
+print(f)
+
+mines.py
+
+#!/usr/bin/python3
+import sys
+
+def factorial(n):
+    """
+    Calculate the factorial of a non-negative integer n using recursion.
+    
+    Parameters:
+    n (int): A non-negative integer for which the factorial is to be calculated.
+    
+    Returns:
+    int: The factorial of the input integer n.
+    """
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n-1)
+
+f = factorial(int(sys.argv[1]))
+print(f)
